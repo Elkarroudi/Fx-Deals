d5960b4fb0970386e7704d640df17a548d7efdd9
Elkarroudi <amineelkarroudi04@proton.me>
2025-01-09 06:38:16 +0100

    Create MongoDB Config

---
+++ src/main/java/org/progresssoft/fxdeals/config/MongoDbConfig.java
@@ -0,0 +1,10 @@
+package org.progresssoft.fxdeals.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoDbConfig {
+
+}
