2598a3438b525162bf4db84e4654bbf073c0b0dc
Elkarroudi <amineelkarroudi04@proton.me>
2025-01-09 06:47:28 +0100

    Create Fail API DTO & Fail Type ENUM

---
+++ src/main/java/org/progresssoft/fxdeals/util/api/fail/FailType.java
@@ -0,0 +1,10 @@
+package org.progresssoft.fxdeals.util.api.fail;
+
+public enum FailType {
+    DUPLICATE_FX_DEAL_ID,
+    BAD_REQUEST,
+    VALIDATION_ERROR,
+    RESOURCE_NOT_FOUND,
+    HTTP_MESSAGE_NOT_READABLE,
+    CONSTRAINT_VIOLATION_ERROR
+}
