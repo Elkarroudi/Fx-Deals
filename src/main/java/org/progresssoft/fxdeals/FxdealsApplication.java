787a5ffc1f3b55b6856714555727a141d76765e1
Elkarroudi <amineelkarroudi04@proton.me>
2025-01-09 08:15:08 +0100

    Create Dtos Group

---
+++ src/main/java/org/progresssoft/fxdeals/FxdealsApplication.java
@@ -0,0 +1,13 @@
+package org.progresssoft.fxdeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxdealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxdealsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/progresssoft/fxdeals/dto/group/OnCreate.java
@@ -0,0 +1,6 @@
+package org.progresssoft.fxdeals.dto.group;
+
+import jakarta.validation.groups.Default;
+
+public interface OnCreate extends Default {
+}
